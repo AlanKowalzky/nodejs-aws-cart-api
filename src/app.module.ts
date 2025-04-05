00ee243e4ec05071b37a7be45eb33e4067442f02
alan.kowalzky <alan.kowalzky@gmail.com>
2025-04-05 08:27:07 +0000

    feat: infra stack

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,33 @@ import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+}
 
 @Module({
-  imports: [AuthModule, CartModule, OrderModule, ConfigModule.forRoot()],
+  imports: [AuthModule, CartModule, OrderModule, ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_user',
+      password: 'your_password',
+      database: 'your_db',
+      entities: [User],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [],
 })

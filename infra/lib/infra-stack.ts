e339ba6521cedda9b154066732bf95c34b2ad63a
alan.kowalzky <alan.kowalzky@gmail.com>
2025-04-05 10:26:36 +0000

    feat: handler wthout api

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Existing VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC', {
+      vpcId: 'vpc-0d16b7ccf79fadb50',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Subnets
+    const subnets = [
+      'subnet-010bdd05db99b0fbd',
+      'subnet-0ca84313c3cacbe19',
+      'subnet-0fa3afd92e0be3d0d',
+      'subnet-08cc09a19b1a369fc',
+      'subnet-0c9e4613837bce057',
+      'subnet-082406b9d0d4e960d',
+    ];
+
+    // Lambda function
+    const lambdaFunction = new lambda.Function(this, 'MyLambdaFunction', { // TODO: change MyLambdaFunction to Your lambda name
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler', // TODO: update path to your handler
+      code: lambda.Code.fromAsset('path/to/your/lambda/code'), // TODO: update path to lambda code
+      vpc: vpc,
+      vpcSubnets: { subnets: subnets.map((subnetId) => ec2.Subnet.fromSubnetId(this, subnetId, subnetId)) },
+      environment: {
+        DATABASE_URL:
+          'postgresql://placeholder:placeholder@database-1.cexgwe8kuwfo.us-east-1.rds.amazonaws.com:5432/', // default db name
+      },
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/models/user.entity';
 import { AppController } from './app.controller';
-
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column()
-  name: string;
-
-  @Column()
-  password: string;
-}
-
 @Module({
-  imports: [AuthModule, CartModule, OrderModule, ConfigModule.forRoot(),
+  imports: [AuthModule, CartModule, OrderModule, ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'database-1.cexgwe8kuwfo.us-east-1.rds.amazonaws.com',
       port: 5432,
       username: 'your_user',
-      password: 'your_password',
-      database: 'your_db',
+      password: process.env.DB_PASSWORD,
       entities: [User],
       synchronize: true,
-    })],
+    })
+  ],
   controllers: [AppController],
   providers: [],
+
 })
 export class AppModule {}
